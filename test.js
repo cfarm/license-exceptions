b754a8613220afc78b9d11bab406ec9aabb06072
Catherine Farman <Catherine.Farman@cfpb.gov>
2016-05-23 15:09:08 -0400

    cleanup package.json + gitignore, add todos for tests

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 var licenseExceptions = require('./index.js');
 
 licenseExceptions.init();
+
+// check for existence of licenseExceptions.md
+
+
+// search file for excluded licenses:
+// exclude: 'CC0-1.0, Public domain, public domain, Public Domain'
