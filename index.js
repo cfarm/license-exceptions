4632a66b4962f9a962fa8d70b7093bb1d688aad9
Catherine Farman <Catherine.Farman@cfpb.gov>
2016-05-24 11:42:58 -0400

    add Exceptions list to TERMS.md file, overwriting Exceptions section each time

--- index.js
+++ index.js
@@ -5,20 +5,18 @@ var checker = require( 'license-checker' );
 
 function getTermsFile( licenses, fileName ) {
   fs.readFile( fileName, 'utf8', function( err, fileContents ) {
-    // console.log( fileContents );
     var licenseOutput = addExceptions( licenses );
-    // console.log( licenseOutput );
+    var exceptionsContent = '## Exceptions';
+    var removeOldExceptions = fileContents.split( exceptionsContent )[0];
+    var newTerms = removeOldExceptions + licenseOutput;
 
-    var newTerms = fileContents + licenseOutput;
-    console.log( newTerms );
-
-    fs.writeFile( 'newTerms.md', newTerms, 'utf8' );
+    fs.writeFile( fileName, newTerms, 'utf8' );
   });
 }
 
 function addExceptions( licenses ) {
 
-  var formattedData = '# License Exceptions\n\n Source code or other assets that are excluded from the [TERMS](TERMS.md). This list includes dependencies that may be licensed differently (not CC0-1.0) or are not in the public domain.\n\n';
+  var formattedData = '## Exceptions\n\n Source code or other assets that are excluded from the terms. This list includes dependencies that may be licensed differently (not CC0-1.0) or are not in the public domain.\n\n';
 
   var licenseList = Object.keys( licenses ).map(
     function( license ) {
